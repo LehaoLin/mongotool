5bf39bc102c231a203d8c9b59308f4914118c679
LehaoLin <lehaolin@link.cuhk.edu.cn>
2024-01-10 20:46:01 +0800

    update

--- config.js
+++ config.js
@@ -6,5 +6,9 @@
 // /etc/mongod.conf
 // bind_ip = 127.0.0.1
 // sudo service mongod restart
+// # network interfaces
+// net:
+// port: 27017
+// bindIp: 127.0.0.1
 export const mongoURI = `mongodb://localhost:27017`;
 export const dbName = "dbname";
